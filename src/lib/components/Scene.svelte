a6dfebcff1e3be590a58e2c9b63c636f505c2ca0
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-10-06 22:21:28 +0530

    whole project in 1

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  //import { T } from '@threlte/core'
+  //import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import Camera from './Camera.svelte'
+  import Planets from './models/planets.svelte'
+
+</script>
+
+<Camera />
+<Planets />
